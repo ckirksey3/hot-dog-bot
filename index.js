0e438e470fabd1a2459faad01244c66ca23665e7
Caleb Kirksey <ckirksey3@gmail.com>
2017-05-16 23:49:30 -0700

    use AWS recognize to determine whether its a hotdog

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // START HEROKU SETUP
 var express = require("express");
 var app = express();
+var AWS = require('aws-sdk');
+AWS.config.update({region: 'us-east-1'});
+var rekognition = new AWS.Rekognition();
+var request = require('request').defaults({ encoding: null });
 app.get('/', function(req, res){ res.send('The robot is happily running.'); });
 app.listen(process.env.PORT || 5000);
 // END HEROKU SETUP
@@ -54,6 +58,17 @@ function onReTweet(err) {
     }
 }
 
+function tweetBasedOnCategorization(tweet, isItAHotdog) {
+    var message = " This is NOT a hotdog";
+    if(isItAHotdog) {
+        message = " This is a hotdog";
+    }
+    tu.update({
+        status: "@" + tweet.user.screen_name + message,
+        in_reply_to_status_id: tweet.id_str
+    }, onReTweet);
+}
+
 // What to do when we get a tweet.
 function onTweet(tweet) {
     // Reject the tweet if:
@@ -73,11 +88,37 @@ function onTweet(tweet) {
         // Note we're using the id_str property since javascript is not accurate
         // for 64bit ints.
         if(tweet.entities.hasOwnProperty('media') && tweet.entities.media.length > 0) {
-            tu.update({
-            status: "@" + tweet.user.screen_name + " Not sure whether this is a hotdog",
-                in_reply_to_status_id: tweet.id_str
-            }, onReTweet);
-            console.log("Image URL: " + tweet.entities.media[0]['media_url'])
+            var image_url = tweet.entities.media[0]['media_url'];
+            request.get(image_url, function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+                    var params = {
+                        Image: { 
+                            Bytes: body
+                        },
+                        MaxLabels: 20,
+                        MinConfidence: 70
+                    };
+                    rekognition.detectLabels(params, function(err, data) {
+                        if (err) console.log(err, err.stack); // an error occurred
+                        else {
+                            console.log(data);           // successful response
+                            var isItAHotdog = false;
+                            for (var label_index in data.Labels) {
+                                var label = data.Labels[label_index];
+                                if(label['Name'] == "Hot Dog") {
+                                   if(label['Confidence'] > 85) {
+                                        isItAHotdog = true;
+                                        tweetBasedOnCategorization(tweet, true);
+                                    }
+                                }
+                            }
+                            if(isItAHotdog == false) {
+                                tweetBasedOnCategorization(tweet, false);
+                            }
+                        }
+                    });
+                }
+            });
         } else {
             console.log("Tweet did not have an image")
         }
